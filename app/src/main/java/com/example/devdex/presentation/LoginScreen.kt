d07e7ed287f597340a7a0f0f415c25b055b0872b
Jessica Ducker <jessicaducker@tabasco.gob.mx>
2026-09-04 16:25:25 -0600

    ffeature/loginScreen

--- app/src/main/java/com/example/devdex/presentation/LoginScreen.kt
+++ app/src/main/java/com/example/devdex/presentation/LoginScreen.kt
@@ -1,10 +1,15 @@
 package com.example.devdex.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,11 +18,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
 
 @Composable
 fun LoginScreen() {
@@ -26,8 +43,28 @@ fun LoginScreen() {
     var password by remember { mutableStateOf("") }
     var isPasswordVisible by remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "DevDex")
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Icon(imageVector = Icons.Filled.Home)
+
+        Text(
+            text = "DevDex",
+            fontSize = 30.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Empieza tu aventura Pokemon local",
+            fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Text(text = "Nombre de usuario")
 
@@ -37,8 +74,12 @@ fun LoginScreen() {
             label = { Text(text = "Nombre de usuario") },
             modifier = Modifier.fillMaxWidth(),
             placeholder = { Text(text = "Ej. Trainer Red") },
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            shape = RoundedCornerShape(16.dp)
         )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(text = "Contraseña")
 
         OutlinedTextField(
             value = password,
@@ -48,9 +89,25 @@ fun LoginScreen() {
             placeholder = { Text(text = "Ej. 123") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            shape = RoundedCornerShape(16.dp)
         )
-        FilledTonalButton(onClick = { }) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        FilledTonalButton(onClick = { }, modifier = Modifier.fillMaxWidth()) {
             Text("LOGIN")
         }
+        Text(
+            text = buildAnnotatedString {
+                append("¿Ya tienes perfil? ")
+                val linkAnnotation = LinkAnnotation.Url(
+                    url = "https://developer.android.com",
+                    styles = TextLinkStyles(style = SpanStyle(color = Color.Blue))
+                )
+                withLink(linkAnnotation) {
+                    append("Iniciar sesión")
+                }
+            }, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth()
+        )
+
     }
 }
